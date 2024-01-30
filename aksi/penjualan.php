f414a8cd1992c8753dde0878dc844d36c0d86ec4
kadekninaantari <kadekninaantari@gmail.com>
2024-01-30 12:20:06 +0700

    Menambah Modul Laporan (Produk,Pelanggan,Penjualan,Penjualan Per-Produk)

--- aksi/penjualan.php
+++ aksi/penjualan.php
@@ -53,14 +53,44 @@ if($_POST){
     }
     else if($_POST['aksi']=='simpan-penjualan'){
         $id_user=$_SESSION['id'];
-        $PelangganID=$_POST['TanggalPenjualan'];
+        $PelangganID=$_POST['PelangganID'];
         $TanggalPenjualan=$_POST['TanggalPenjualan'];
         $TotalHarga=$_POST['TotalHarga'];
 
         $sql1="INSERT INTO penjualan(PenjualanID,TanggalPenjualan,TotalHarga,PelangganID) values(Default,'$TanggalPenjualan',$TotalHarga,$PelangganID)";
        // echo $sql1;
        if(mysqli_query($koneksi,$sql1)){
-        echo "Simpan penjualan sukses";
+        // echo "Simpan penjualan sukses";
+        // Mengambil PenjualanID dari tabel penjualan
+        $sql2="SELECT MAX(PenjualanID) AS LastID FROM penjualan";
+        $query2=mysqli_query($koneksi,$sql2);
+        $data=mysqli_fetch_array($query2);
+        $PenjualanID=$data['LastID'];
+       // echo $PenjualanID;
+
+        // Menyimpan Data Produk Yang Di Beli ke tabel detail penjualan yang diambil dari tabel keranjang
+        $sql3="SELECT keranjang.*,produk.Harga FROM keranjang,produk WHERE keranjang.ProdukID=produk.ProdukID AND id_user=$id_user";
+       // echo $sql3;
+
+        $query3=mysqli_query($koneksi,$sql3);
+        while($keranjang=mysqli_fetch_array($query3)){
+            $ProdukID=$keranjang['ProdukID'];
+            $Jumlah=$keranjang['Jumlah'];
+            $Harga=$keranjang['Harga'];
+
+            $sql4="INSERT INTO detailpenjualan(DetailID,PenjualanID,ProdukID,JumlahProduk,Harga) values (default,$PenjualanID,$ProdukID,$Jumlah,$Harga)";
+           // echo $sql4."<br>";
+           mysqli_query($koneksi,$sql4);
+
+           // Mengurangi Nilai Stok
+           $sql5="UPDATE produk SET Stok=Stok-$Jumlah WHERE ProdukID=$ProdukID";
+           mysqli_query($koneksi,$sql5);
+        }
+        // Perintah mengosongkan keranjang
+        mysqli_query($koneksi,"DELETE FROM keranjang WHERE id_user=$id_user");
+        notifikasi($koneksi);
+        header('location:../index.php?p=tambah');
+
        }
     }
 }
@@ -74,5 +104,17 @@ if($_GET){
         notifikasi($koneksi);
 
         header('location:../index.php?p=tambah');
+}
+else if ($_GET['aksi']=='hapus'){
+        $PenjualanID=$_GET['PenjualanID'];
+        $sql1="DELETE FROM penjualan WHERE PenjualanID=$PenjualanID";
+        mysqli_query($koneksi,$sql1);
+
+        $sql2="DELETE FROM detailpenjualan WHERE PenjualanID=$PenjualanID";
+        mysqli_query($koneksi,$sql2);
+
+        
+        notifikasi($koneksi);
+        header('location:../index.php?p=histori');
 }
 }
